4cccfe43d460cb4f07d03f1def29873184349e01
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2022-12-30 12:26:27 +0100

    Add files via upload

---
+++ Assignment_3/build/CMakeFiles/assignment4.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/ricky/OneDrive/LM_CGAR/Computer_Graphics/Assignements/Assignment_3/src/main.cpp" "/mnt/c/Users/ricky/OneDrive/LM_CGAR/Computer_Graphics/Assignements/Assignment_3/build/CMakeFiles/assignment4.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/mnt/c/Users/ricky/OneDrive/LM_CGAR/Computer_Graphics/Assignements/Assignment_3/data/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../ext/eigen"
+  "../../ext/stb"
+  "../../ext/gif-h"
+  "../../ext/json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
